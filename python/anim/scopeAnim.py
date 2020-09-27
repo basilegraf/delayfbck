29d2063789a9f7f9a6a085c5b6049e58671b90b0
Basile <basile.graf@a3.epfl.ch>
2020-09-27 15:08:40 +0200

    Full HD with black background OK

--- python/anim/scopeAnim.py
+++ python/anim/scopeAnim.py
@@ -29,6 +29,10 @@ saveAnimation = True
 
 plt.close('all')
 
+fps = 60
+rate = 44100
+nPerFrame = 300
+
 x = np.linspace(0,1000,50000);
 y = np.cos(x) * x
 
@@ -44,9 +48,9 @@ class animScope:
         nFrames = int(fps * (len(self.data)-self.npf) / sr)
         self.frames = range(0, nFrames)
         self.fig, self.ax = plt.subplots()
-        self.fig.tight_layout()
+        
         self.ax.clear()
-        self.ln, = self.ax.plot([], [])
+        self.ln, = self.ax.plot([], [],linewidth=6)
         self.ax.set_aspect(aspect='auto', adjustable='box')
         self.ax.set_xlim(left=0, right=self.npf)
         self.ax.set_ylim(bottom=-0.52, top=0.52)
@@ -54,37 +58,32 @@ class animScope:
         self.ax.set_ybound(lower=-0.52, upper=0.52)
         self.ax.grid(b=False)
         self.ax.axis('off')
-        self.fig.patch.set_facecolor('xkcd:black')
+        self.fig.patch.set_facecolor((0,0,0))
+        self.fig.patch.set
         self.ax.set_facecolor((0,0,0))
         
         self.fig.set_size_inches(19.20, 10.80, True)
         self.fig.set_dpi(100)
+        self.fig.tight_layout()
     def initAnim(self):
         print('meuh')
         
 
     def update(self, frame):
-        print(frame)
         n = len(self.data) - self.npf
         idx = int(np.floor(frame/len(self.frames) * n))
         self.ln.set_xdata(range(0,self.npf))
         self.ln.set_ydata(self.data[idx : idx+self.npf])
-        print(idx, idx+self.npf)
        
     def anim(self):
         return FuncAnimation(self.fig, self.update, self.frames, init_func=self.initAnim, blit=False, repeat=False, interval=1000/self.fps)
         
 
-scope = animScope(y, 300, 25, 44100)
+scope = animScope(y, nPerFrame, fps, rate)
 aa = scope.anim()
 
-#if saveAnimation:
-#    aa.save('beforeOptim.gif', writer='imagemagick', fps=25)
 
-#Writer = animation.writers['ffmpeg']
-#writer = Writer(fps=25, metadata=dict(artist='Ugarte'), bitrate=1000)
+writer = animation.FFMpegWriter(fps=fps, metadata=dict(artist='Ugarte'), bitrate=3000)
 
-writer = animation.FFMpegWriter(fps=25, codec=None, bitrate=3000 , metadata=None)
-
-aa.save('HeroinOverdosesJumpy.mp4', writer=writer,dpi=100)
+aa.save('SlowFactory.mp4', writer=writer,dpi=100, savefig_kwargs=dict(facecolor=(0,0,0)))
 
